f6e6dd1edcbd80da1476fdb8980ad14019ccb57b
Stefano Baviello <stefano.baviello@teoresigroup.com>
2022-11-14 09:13:40 +0100

    Initial commit

---
+++ src/test/java/com/teoresi/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teoresi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
